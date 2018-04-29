484cb9cb587b83b0778beb0ca41efc477f254b33
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-04-29 23:13:30 +0530

    added freestyle demo

---
+++ src/app/choosy-demo/_data/places.ts
@@ -0,0 +1,30 @@
+export const places = [
+  {
+    name: 'icon-pisa',
+    title: 'Pisa Tower'
+  },
+  {
+    name: 'icon-eiffel',
+    title: 'Eiffel Tower'
+  },
+  {
+    name: 'icon-tajmahal',
+    title: 'Taj Mahal'
+  },
+  {
+    name: 'icon-building',
+    title: 'Buildings'
+  },
+  {
+    name: 'icon-gate',
+    title: 'Golden gate'
+  },
+  {
+    name: 'icon-burj',
+    title: 'Burj Khalifa'
+  },
+  {
+    name: 'icon-beach',
+    title: 'Beach'
+  }
+];
--- src/app/choosy-demo/iconic/iconic.component.ts
+++ src/app/choosy-demo/iconic/iconic.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ChoosyDemoBaseComponent } from '../base';
+import { places } from '../_data/places';
 
 @Component({
   selector: 'doc-iconic',
   templateUrl: './iconic.component.html',
   styles: []
 })
-export class IconicComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class IconicComponent extends ChoosyDemoBaseComponent implements OnInit {
+  options = places;
+  ngOnInit() {}
 }
